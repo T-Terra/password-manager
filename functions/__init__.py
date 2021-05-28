23ffcbb2f822a1314e9f7424ac61b8f0f97ceff1
Gabriel <gabst138@gmail.com>
2021-05-27 22:21:32 -0300

    updade project

---
+++ functions/__init__.py
@@ -0,0 +1,5 @@
+def cripto(msg=""):
+    main_alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    main_char = ['!', '@', '#']
+    for alpha in main_alpha:
+        print(f'{alpha}')
---
+++ main.py
@@ -0,0 +1,3 @@
+from functions import cripto
+
+cripto()
