43a45a06b67e6ff139d9013af6135ff8c268bf20
Gabriel <gabst138@gmail.com>
2021-06-16 23:57:46 -0300

    update exe

--- library/interface/__init__.py
+++ library/interface/__init__.py
@@ -1,10 +1,15 @@
+def response():
+    global resp
+    resp = int(input('Sua opção: '))
+
+
 def menu():
     from time import sleep
     while True:
         main_menu = ['Recadastrar senha mestra', 'Listar senhas', 'Sair do programa']
         for k, v in enumerate(main_menu):
             print(f'{k+1} - {v}')
-        resp = int(input('Sua opção: '))
+        response()
         if resp == 3:
             print(20 * '=')
             print('Saindo do programa...')
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from library.interface import menu
+from library.interface import menu, response
 
 # Main program
 password = str(input('Cadastre a senha mestra: '))
