bb35af416af86010a94ee7379373f5c3c0d4f102
Gabriel <gabst138@gmail.com>
2021-06-16 23:42:39 -0300

    Update __init__.py

--- library/interface/__init__.py
+++ library/interface/__init__.py
@@ -1,4 +1,13 @@
 def menu():
-    main_menu = ['Recadastrar senha mestra', 'Listar senhas', 'Sair do programa']
-    for k, v in enumerate(main_menu):
-        print(f'{k+1} - {v}')
+    from time import sleep
+    while True:
+        main_menu = ['Recadastrar senha mestra', 'Listar senhas', 'Sair do programa']
+        for k, v in enumerate(main_menu):
+            print(f'{k+1} - {v}')
+        resp = int(input('Sua opção: '))
+        if resp == 3:
+            print(20 * '=')
+            print('Saindo do programa...')
+            sleep(2)
+            break
+        
