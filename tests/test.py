8cc25cd6008d616c5245ae03cd702580956b3bc6
Gabriel <gabst138@gmail.com>
2021-07-03 14:48:41 -0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -1,22 +1,6 @@
-import lib.interface
+from lib.interface import *
 
 # Main program
-lib.interface.connect_server()
-lib.interface.register_pass_master()
-lib.interface.pass_validation()
-
-'''from hashlib import sha256
-
-
-def cryptography(pass_cryp=''):
-    crypt = sha256()
-    crypt.update(str.encode(pass_cryp))
-    c = crypt.hexdigest()
-    return print(c)
-
-cryptography(input('senha: '))
-
-crypt = sha256()
-crypt.update(b'1234')
-c = crypt.hexdigest()
-print(c)'''
+connect_server()
+register_pass_master()
+pass_validation()
