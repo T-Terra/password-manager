58fe151af7b7953ff5c693b44eeea629a3b98856
Gabriel <gabst138@gmail.com>
2021-07-10 13:32:08 -0300

    up

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from lib.src import pass_validation, connect_server, register_pass_master, screen
+from lib.src import pass_validation, connect_server, register_pass_master
+from lib.screen import screen
 
 # Main program
 screen()
 connect_server()
 register_pass_master()
+pass_validation()
