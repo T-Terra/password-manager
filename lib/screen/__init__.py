94c9df17a3abbd20fc2c9caa4e872017ba3055b5
Gabriel <gabst138@gmail.com>
2021-07-18 17:11:57 -0300

    upgrade program

--- lib/screen/__init__.py
+++ lib/screen/__init__.py
@@ -15,7 +15,7 @@ def screen_login():
     window.wm_resizable(width=False, height=False) # para não almentar a tela com o mouse 
     # mapeando o cursor do mouse e o tamanho da janela
     #window.bind('<Button-1>', cli_esq_mouse)
-    input_user = Entry(window, font='Arial 13')
+    input_user = Entry(window, font='Arial 13', show='*')
     input_user.grid(column=1, row=0)
     txt = Label(window, text='Senha mestra: ', font='Arial 14')
     txt.grid(column=0, row=0, padx=25, pady=10, )
@@ -28,6 +28,7 @@ def screen_login():
 def screen_menu():
     window2 = Tk()
     window2.title('Gerenciador de Senhas')
+    window2.iconphoto(False, PhotoImage(file='./img/seguranca.ico'))
     window2.geometry('550x400+380+150')
     window2.wm_resizable(width=False, height=False)
     window2.mainloop()
@@ -40,6 +41,7 @@ def send_response():
 # Manda a condição para o back-end
 def send_label(arg=0):
     if arg == 1:
-        error['text'] = 'Senha correta!'
+        window.destroy()
+        screen_menu()
     elif arg == 0:
         error['text'] = 'Senha inválida!'
